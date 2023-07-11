e8ffa816dad22d31c92cf792203c94b558e8ba8d
Sarthak Kashyap <98842340+ROOKIEsarthak@users.noreply.github.com>
2023-07-12 02:45:14 +0530

    commmit 1

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import { UserContext } from "../UserContext"
-import { Navigate } from "react-router"
+import { Navigate } from "react-router-dom"
+import {Link} from "react-router-dom"
 
 export default function AccountPage() {
 
@@ -18,6 +19,15 @@ export default function AccountPage() {
   
 
   return (
-    <div>Account PAGE for {user.name}</div>
+    <div>
+      <nav className="w-full flex justify-center mt-8 gap-2 ">
+        <Link className="py-2 px-6 bg-primary rounded-full " to={'/account'}> My Profile </Link>
+        <Link className="py-2 px-6 " to={'/accounts/bookings'}> My bookings</Link>
+        <Link className="py-2 px-6 " to={'/accounts/places'}> My accommodations</Link>
+      </nav>
+
+    </div>
+    
+    
   )
 }
