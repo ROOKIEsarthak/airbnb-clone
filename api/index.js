c4ec819cafb73807ae532395d020498748f906c2
Sarthak Kashyap <98842340+ROOKIEsarthak@users.noreply.github.com>
2023-07-21 08:12:56 +0530

    added new place done

--- api/index.js
+++ api/index.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
 const mongoose = require('mongoose');
+const multer = require('multer');
 const app = express();
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const User = require('./models/user')
+const Place = require('./models/place');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const imageDownloader = require('image-downloader');
+const { log } = require('console');
+const fs = require('fs');
+
 
 const bcryptSalt =  bcrypt.genSaltSync(10);
 const jwtSecret = 'asdadadkasjdbakduadhiauwnkq'
 app.use(express.json())
 
 app.use(cookieParser())
+app.use('/uploads' , express.static(path.join(__dirname , 'uploads')));
+
 
 app.use(cors({
     credentials: true,
@@ -103,5 +113,108 @@ app.get('/profile' , (req,res)=>{
     
     
 })
+
+app.post('/logout' , (req,res) => {
+    res.cookie('token' , '').json(true)
+})
+
+
+
+app.post('/upload-by-link' , async (req,res) => {
+    
+    
+    const {link} = req.body
+    
+    const newName ='photo'+ Date.now() +'.jpg';
+
+   
+    await imageDownloader.image({
+        url : link,
+        dest : __dirname + '/uploads/' + newName 
+    });
+
+    res.json(newName)
+})
+
+const photosMiddleware = multer({dest:'uploads/'})
+
+app.post('/upload',photosMiddleware.array('photos' , 100) ,(req,res) =>{
+
+    const uploadedFiles = [];
+    for (let i= 0 ; i < req.files.length ; i++)
+    {
+        const {path , originalname} = req.files[i]
+        const parts = originalname.split('.')
+        const ext = parts[parts.length - 1];
+        const newPath = path + '.' + ext;
+        fs.renameSync(path , newPath)
+        uploadedFiles.push(newPath.replace('uploads\\',''));
+    }
+    res.json(uploadedFiles)
+})
+
+
+app.post('/places' , (req,res) => {
+    const {token} = req.cookies ; 
+    const {
+        title , address , addedPhotos , description,
+        perks , extraInfo , checkIn , checkOut , maxGuests } = req.body;
+
+    jwt.verify(token , jwtSecret ,{} , async(err,userData)=>{
+        if(err) throw err;
+        const placeDoc = await Place.create({
+            owner:userData.id,
+            title , address , photos:addedPhotos , description,
+            perks , extraInfo , checkIn , checkOut , maxGuests
+        });
+        res.json(placeDoc)
+    })
+    
+    
+})
+
+
+app.get('/places' , (req,res)=>{
+    const {token} = req.cookies ; 
+    jwt.verify(token , jwtSecret ,{} , async(err,userData)=>{
+        const {id} = userData;
+        res.json( await Place.find({owner:id}));
+
+    });
+});
+
+app.get('/places/:id' , async(req,res) => {
+    
+    const{id} = req.params
+    res.json(await Place.findById(id) );
+})
+
+app.put('/places', async(req,res)=>{
+    
+    const {token} = req.cookies ; 
+    const { id,
+        title , address , addedPhotos , description,
+        perks , extraInfo , checkIn , checkOut , maxGuests 
+    } = req.body; 
+    
+    jwt.verify(token , jwtSecret ,{} , async(err,userData)=>{
+        if(err) throw err;
+
+        
+       
+        
+        const placeDoc = await Place.findById(id);
+
+        if(userData.id === placeDoc.owner.toString()){
+            placeDoc.set({
+            title , address , photos:addedPhotos , description,
+            perks , extraInfo , checkIn , checkOut , maxGuests
+
+            })
+            await placeDoc.save()
+            res.json({placeDoc})
+        }
+    })
+    })
 
 app.listen(4000);
